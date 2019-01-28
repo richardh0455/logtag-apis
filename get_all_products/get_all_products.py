2be578ecc5567cf0ba49d4d5ce3d7c472bc12701
Healdr <Richard.Heald@airnz.co.nz>
2019-01-29 12:28:14 +1300

    Added APIs for other common operations
    Elided db details
    
    renamed main.py to be more representative

---
+++ get_all_products/get_all_products.py
@@ -0,0 +1,38 @@
+import postgresql
+import json
+
+host  = "<ELIDED>"
+port = '<ELIDED>'
+username = "<ELIDED>"
+password = "<ELIDED>"
+db_name = "<ELIDED>"
+
+def done(response):
+    return {
+        'statusCode': '200',
+        'body': response,
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Methods': '*',
+            'Access-Control-Allow-Origin': '*'
+        }
+    }
+
+def lambda_handler(event, context):
+    db = postgresql.open('pq://' + username + ':' + password + '@' + host + ':' + port + '/' + db_name)
+    cursor = db.prepare("SELECT \"ProductID\", \"Name\" FROM public.\"Products\"")
+    list = []
+    for row in cursor:
+        list.append(parse_row(row))
+    result = '['    
+    result += ','.join(list)
+    result += ']'
+    return done(result)
+    
+def parse_row(row):
+    result = '{'
+    items = list(row.items())
+    result += "\"ID\":\""+str(items[0][1])+"\","
+    result += "\"Name\":\""+items[1][1]+"\""
+    result += '}'
+    return result  
---
+++ get_customer/get_customer.py
@@ -0,0 +1,49 @@
+import postgresql
+import json
+
+host  = "<ELIDED>"
+port = '<ELIDED>'
+username = "<ELIDED>"
+password = "<ELIDED>"
+db_name = "<ELIDED>"
+
+def done(response):
+    return {
+        'statusCode': '200',
+        'body': response,
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Methods': '*',
+            'Access-Control-Allow-Origin': '*'
+        }
+    }
+def fail():
+    return {
+        'statusCode': '406',
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Methods': '*',
+            'Access-Control-Allow-Origin': '*'
+        }
+    }    
+
+def lambda_handler(event, context):
+    print('Received event: '+json.dumps(event))
+    db = postgresql.open('pq://' + username + ':' + password + '@' + host + ':' + port + '/' + db_name)
+    try:
+        id = int(event["id"])
+    except:
+        return fail()
+    cursor = db.prepare("SELECT * FROM public.\"Customer\" WHERE \"CustomerID\"="+str(id))
+    list = []
+    for row in cursor:
+        list.append(parse_row(row))
+    result = ','.join(list)
+    return done(result)
+    
+def parse_row(row):
+    result = '{'
+    for item in row.items():
+        result += str(item[0]) + ':\"' + str(item[1]) + '\",'
+    result += '}'
+    return result    
