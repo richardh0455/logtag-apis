99417271ef645e4f740ea27e660264a805ab4e1e
healdr <richardheald335@gmail.com>
2019-03-20 17:46:51 +1300

    Added new Lambda get_all_variations

---
+++ get_all_variations/get_all_variations.py
@@ -0,0 +1,48 @@
+import psycopg2
+import json
+import os
+
+host  = os.environ['RDS_HOST']
+port = os.environ['PORT']
+username = os.environ['USERNAME']
+password = os.environ['PASSWORD']
+db_name = os.environ['DB_NAME']
+connection = psycopg2.connect(user=username,
+                              password=password,
+                              host=host,
+                              port=port,
+                              database=db_name)
+
+
+def done(response):
+    return {
+        'statusCode': '200',
+        'body': response,
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Methods': '*',
+            'Access-Control-Allow-Origin': '*'
+        }
+    }
+
+def lambda_handler(event, context):
+    cursor = connection.cursor()
+    customer_id = event["CustomerID"]
+    product_id = event["ProductID"]
+    cursor.execute("SELECT \"Description\", \"Price\" FROM public.\"ProductConfiguration\" WHERE \"CustomerID\"= %s AND \"ProductID\"= %s",(str(customer_id),str(product_id),))
+    list = []
+    for row in cursor.fetchall():
+        list.append(parse_row(row))
+    result = '['    
+    result += ','.join(list)
+    result += ']'
+    connection.commit()
+    cursor.close()
+    return done(result)
+    
+def parse_row(row):
+    result = '{'
+    result += "\"Description\":\""+row[0]+"\","
+    result += "\"Price\":\""+str(row[1])+"\""
+    result += '}'
+    return result  
