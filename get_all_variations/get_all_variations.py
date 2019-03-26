fc9d4770ae458325da920c4fa660b04fff45bf3f
Healdr <Richard.Heald@airnz.co.nz>
2019-03-27 12:13:58 +1300

    Added an API to update a shipping Address

--- get_all_variations/get_all_variations.py
+++ get_all_variations/get_all_variations.py
@@ -29,20 +29,21 @@ def lambda_handler(event, context):
     cursor = connection.cursor()
     customer_id = event["CustomerID"]
     product_id = event["ProductID"]
-    cursor.execute("SELECT \"Description\", \"Price\" FROM public.\"ProductConfiguration\" WHERE \"CustomerID\"= %s AND \"ProductID\"= %s",(str(customer_id),str(product_id),))
+    cursor.execute("SELECT \"ConfigurationID\", \"Description\", \"Price\" FROM public.\"ProductConfiguration\" WHERE \"CustomerID\"= %s AND \"ProductID\"= %s",(str(customer_id),str(product_id),))
     list = []
     for row in cursor.fetchall():
         list.append(parse_row(row))
-    result = '['    
+    result = '['
     result += ','.join(list)
     result += ']'
     connection.commit()
     cursor.close()
     return done(result)
-    
+
 def parse_row(row):
     result = '{'
-    result += "\"Description\":\""+row[0]+"\","
-    result += "\"Price\":\""+str(row[1])+"\""
+    result += "\"VariantID\":\""+str(row[0])+"\","
+    result += "\"Description\":\""+row[1]+"\","
+    result += "\"Price\":\""+str(row[2])+"\""
     result += '}'
     return result  
