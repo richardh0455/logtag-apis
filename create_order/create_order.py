ac2843045280b463bd5bf0769e75a28b47270be5
Healdr <Richard.Heald@airnz.co.nz>
2019-03-29 10:20:58 +1300

    Added LogtagInvoiceNumber to createOrder API

--- create_order/create_order.py
+++ create_order/create_order.py
@@ -1,6 +1,7 @@
 import psycopg2
 import json
 import os
+from datetime import datetime
 
 host  = os.environ['RDS_HOST']
 port = os.environ['PORT']
@@ -25,24 +26,24 @@ def lambda_handler(event, context):
                                   host=host,
                                   port=port,
                                   database=db_name)
-    cursor = connection.cursor()                              
+    cursor = connection.cursor()
     invoice_id = create_invoice(cursor, int(event["customerID"]))
     create_invoice_items(cursor, invoice_id, event["invoiceLines"])
     connection.commit()
     cursor.close()
     connection.close()
     return done(json.dumps('{ \"InvoiceID\":\"'+str(invoice_id)+'\"}'))
-    
+
 def create_invoice(cursor, customerID):
-    cursor.execute("INSERT into public.\"Invoice\" (\"CustomerID\")  VALUES ( %s ) RETURNING \"InvoiceID\"", [customerID])
+    logtagInvoiceNumber = 'IN'+datetime.now().strftime("%d%m%y")
+    cursor.execute("INSERT into public.\"Invoice\" (\"CustomerID\", \"LogtagInvoiceNumber\")  VALUES ( %s, %s ) RETURNING \"InvoiceID\"", [customerID, logtagInvoiceNumber])
     row = cursor.fetchone()
     return row[0]
 
 def create_invoice_items(cursor, invoiceID, invoiceLines):
-    
+
     for line in invoiceLines:
         variationID = line["VariationID"]
         if(line["VariationID"] == "NULL"):
             variationID = "0"
-        cursor.execute("INSERT into public.\"InvoiceLine\" (\"InvoiceID\", \"Quantity\", \"ProductID\", \"Pricing\", \"VariationID\")  VALUES ( %s, %s, %s, %s, %s )", (invoiceID, int(line["Quantity"]), int(line["ProductID"]), int(line["Price"]),int(variationID)))    
-    
+        cursor.execute("INSERT into public.\"InvoiceLine\" (\"InvoiceID\", \"Quantity\", \"ProductID\", \"Pricing\", \"VariationID\")  VALUES ( %s, %s, %s, %s, %s )", (invoiceID, int(line["Quantity"]), int(line["ProductID"]), int(line["Price"]),int(variationID)))
