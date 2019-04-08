16afe398a3cabe1ab7e0514d52507ea500444482
Healdr <Richard.Heald@airnz.co.nz>
2019-04-09 08:31:11 +1200

    Handling Decimals for prices

--- create_order/create_order.py
+++ create_order/create_order.py
@@ -2,6 +2,7 @@ import psycopg2
 import json
 import os
 from datetime import datetime
+from decimal import Decimal
 
 host  = os.environ['RDS_HOST']
 port = os.environ['PORT']
@@ -19,7 +20,16 @@ def done(response):
             'Access-Control-Allow-Origin': '*'
         }
     }
-
+def fail():
+    return {
+        'statusCode': '406',
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Methods': '*',
+            'Access-Control-Allow-Origin': '*'
+        }
+    }
+    
 def lambda_handler(event, context):
     connection = psycopg2.connect(user=username,
                                   password=password,
@@ -28,9 +38,11 @@ def lambda_handler(event, context):
                                   database=db_name)
     cursor = connection.cursor()
     logtagInvoiceNumber = 'IN'+datetime.now().strftime("%d%m%y")
-    invoice_id = create_invoice(cursor, int(event["customerID"]), logtagInvoiceNumber)
-
-    create_invoice_items(cursor, invoice_id, event["invoiceLines"])
+    try:
+        invoice_id = create_invoice(cursor, int(event["customerID"]), logtagInvoiceNumber)
+        create_invoice_items(cursor, invoice_id, event["invoiceLines"])
+    except:
+        return fail()
     connection.commit()
     cursor.close()
     connection.close()
@@ -48,4 +60,4 @@ def create_invoice_items(cursor, invoiceID, invoiceLines):
         variationID = line["VariationID"]
         if(line["VariationID"] == "NULL"):
             variationID = "0"
-        cursor.execute("INSERT into public.\"InvoiceLine\" (\"InvoiceID\", \"Quantity\", \"ProductID\", \"Pricing\", \"VariationID\")  VALUES ( %s, %s, %s, %s, %s )", (invoiceID, int(line["Quantity"]), int(line["ProductID"]), int(line["Price"]),int(variationID)))
+        cursor.execute("INSERT into public.\"InvoiceLine\" (\"InvoiceID\", \"Quantity\", \"ProductID\", \"Pricing\", \"VariationID\")  VALUES ( %s, %s, %s, %s, %s )", (invoiceID, int(line["Quantity"]), int(line["ProductID"]), Decimal(line["Price"]),int(variationID)))
