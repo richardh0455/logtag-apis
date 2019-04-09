88185bbc7e4e93758d934ef4c97d1f9ff58c7b4f
Healdr <Richard.Heald@airnz.co.nz>
2019-04-10 08:45:12 +1200

    Added API to retrieve all orders

---
+++ get_all_orders/get_all_orders.py
@@ -0,0 +1,59 @@
+import psycopg2
+import json
+import os
+
+host  = os.environ['RDS_HOST']
+port = os.environ['PORT']
+username = os.environ['USERNAME']
+password = os.environ['PASSWORD']
+db_name = os.environ['DB_NAME']
+connection = psycopg2.connect(user=username,
+                              password=password,
+                              host=host,
+                              port=port,
+                              database=db_name)
+
+
+def done(response):
+    return {
+        'statusCode': '200',
+        'body': response,
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Methods': '*',
+            'Access-Control-Allow-Origin': '*'
+        }
+    }
+def fail():
+    return {
+        'statusCode': '406',
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Methods': '*',
+            'Access-Control-Allow-Origin': '*'
+        }
+    }
+
+def lambda_handler(event, context):
+    cursor = connection.cursor()
+    statement = "SELECT \"CustomerID\", \"Name\" FROM public.\"Invoice\""
+    if event["CustomerID"] != null && event["CustomerID"].trim() != "" :
+        statement += " WHERE \"CustomerID\"="+event["CustomerID"]
+
+    cursor.execute(statement)
+    list = []
+    for row in cursor.fetchall():
+        list.append(parse_row(row))
+    result = '['
+    result += ','.join(list)
+    result += ']'
+    connection.commit()
+    cursor.close()
+    return done(result)
+
+def parse_row(row):
+    result = '{'
+    result += "\"ID\":\""+str(row[0])+"\","
+    result += "\"Name\":\""+row[1]+"\""
+    result += '}'
+    return result
