9f6ac1adfada9bf854dbb26cc0b9e22bda0109a1
healdr <richardheald335@gmail.com>
2019-04-26 11:59:26 +1200

    Added API to update variation

---
+++ update_variation/update_variation.py
@@ -0,0 +1,51 @@
+import psycopg2
+import json
+import os
+from decimal import Decimal
+host  = os.environ['RDS_HOST']
+port = os.environ['PORT']
+username = os.environ['USERNAME']
+password = os.environ['PASSWORD']
+db_name = os.environ['DB_NAME']
+
+def done(response):
+    return {
+        'statusCode': '200',
+        'body': response,
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Methods': '*',
+            'Access-Control-Allow-Origin': '*'
+        }
+    }
+def fail():
+    return {
+        'statusCode': '406',
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Methods': '*',
+            'Access-Control-Allow-Origin': '*'
+        }
+    }
+
+def lambda_handler(event, context):
+    connection = psycopg2.connect(user=username,
+                                  password=password,
+                                  host=host,
+                                  port=port,
+                                  database=db_name)
+    updated_rows = 0
+    try:
+        cursor = connection.cursor()
+        update_variation(cursor, event["Description"], event["CustomerID"],event["ProductID"],Decimal(event["CostPrice"]),event["ConfigurationID"] )
+        connection.commit()
+        updated_rows = cursor.rowcount
+        cursor.close()
+        connection.close()
+    except:
+        return fail()
+
+    return done({"AffectedRows":updated_rows})
+
+def update_variation(cursor, description, customer_id, product_id, price, configuration_id):
+    cursor.execute("UPDATE public.\"ProductConfiguration\" SET \"Description\"= %s, \"CustomerID\"= %s, \"ProductID\"= %s, \"Price\"= %s WHERE \"ConfigurationID\"= %s ", (description, customer_id, product_id, price, configuration_id))
