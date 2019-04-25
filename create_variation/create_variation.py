06082e87f429955838a0d48d184983a0670eb3a2
healdr <richardheald335@gmail.com>
2019-04-25 17:39:32 +1200

    Added failure handling for create_variation

--- create_variation/create_variation.py
+++ create_variation/create_variation.py
@@ -1,7 +1,7 @@
 import psycopg2
 import json
 import os
-
+from decimal import Decimal
 host  = os.environ['RDS_HOST']
 port = os.environ['PORT']
 username = os.environ['USERNAME']
@@ -24,22 +24,27 @@ def done(response):
             'Access-Control-Allow-Origin': '*'
         }
     }
+def fail():
+    return {
+        'statusCode': '406',
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Methods': '*',
+            'Access-Control-Allow-Origin': '*'
+        }
+    }
 
 def lambda_handler(event, context):
-    cursor = connection.cursor()
-    customer_id = event["CustomerID"]
-    product_id = event["ProductID"]
-    description = event["Description"]
-    price = event["Price"]
-    cursor.execute("INSERT INTO public.\"ProductConfiguration\" (\"ProductID\", \"CustomerID\", \"Description\", \"Price\") VALUES(%s, %s, %s, %s) RETURNING \"ConfigurationID\"",(str(product_id), str(customer_id),str(description),str(price),))
-    row = cursor.fetchone()[0]
-    connection.commit()
-    cursor.close()
-    return done(json.dumps('{ \"VariantID\":\"'+str(row)+'\"}'))
-
-def parse_row(row):
-    result = '{'
-    result += "\"Description\":\""+row[0]+"\","
-    result += "\"Price\":\""+str(row[1])+"\""
-    result += '}'
-    return result
+    try:
+        cursor = connection.cursor()
+        customer_id = event["CustomerID"]
+        product_id = event["ProductID"]
+        description = event["Description"]
+        price = event["Price"]
+        cursor.execute("INSERT INTO public.\"ProductConfiguration\" (\"ProductID\", \"CustomerID\", \"Description\", \"Price\") VALUES(%s, %s, %s, %s) RETURNING \"ConfigurationID\"",(str(product_id), str(customer_id),str(description),Decimal(price),))
+        row = cursor.fetchone()[0]
+        connection.commit()
+        cursor.close()
+        return done(json.dumps('{ \"VariantID\":\"'+str(row)+'\"}'))
+    except:
+        return fail()
