c6f16707f2a9774e1011e4fda017b17c6c9c25b6
Healdr <Richard.Heald@airnz.co.nz>
2019-05-03 14:08:07 +1200

    Added Update Order API

---
+++ update_order/update_order.py
@@ -0,0 +1,51 @@
+import psycopg2
+import json
+import os
+from decimal import Decimal
+host  = os.environ['RDS_HOST']
+port = os.environ['PORT']
+username = os.environ['USERNAME']
+password = os.environ['PASSWORD']
+db_name = os.environ['DB_NAME']
+
+def done(response):
+    return {
+        'statusCode': '200',
+        'body': response,
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Methods': '*',
+            'Access-Control-Allow-Origin': '*'
+        }
+    }
+def fail():
+    return {
+        'statusCode': '406',
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Methods': '*',
+            'Access-Control-Allow-Origin': '*'
+        }
+    }
+
+def lambda_handler(event, context):
+    connection = psycopg2.connect(user=username,
+                                  password=password,
+                                  host=host,
+                                  port=port,
+                                  database=db_name)
+    updated_rows = 0
+    try:
+        cursor = connection.cursor()
+        update_order(cursor, event["OrderID"], event.get("ShippingDate",""),event.get("purchaseDate",""))
+        connection.commit()
+        updated_rows = cursor.rowcount
+        cursor.close()
+        connection.close()
+    except:
+        return fail()
+
+    return done({"AffectedRows":updated_rows})
+
+def update_order(cursor, orderID, shippingDate, purchaseDate):
+    cursor.execute("UPDATE public.\"Invoice\" SET \"ShippingDate\"= %s, \"purchaseDate\"= %s WHERE \"InvoiceID\"= %s ", (shippingDate, purchaseDate, orderID))
