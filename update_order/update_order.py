8142768518d4285a8e91f73cf3bc06dc571385a9
healdr <richardheald335@gmail.com>
2019-05-04 16:09:29 +1200

    Handling when PaymentDate or Shipping Date is not included in the request

--- update_order/update_order.py
+++ update_order/update_order.py
@@ -2,6 +2,8 @@ import psycopg2
 import json
 import os
 from decimal import Decimal
+from psycopg2.extensions import AsIs
+
 host  = os.environ['RDS_HOST']
 port = os.environ['PORT']
 username = os.environ['USERNAME']
@@ -37,15 +39,22 @@ def lambda_handler(event, context):
     updated_rows = 0
     try:
         cursor = connection.cursor()
-        update_order(cursor, event["OrderID"], event.get("ShippingDate",""),event.get("PurchaseDate",""))
+        shipping_date = event.get("ShippingDate","")
+        if shipping_date.strip():
+            update_order(cursor, event["OrderID"],'ShippedDate',shipping_date )
+
+        purchase_date = event.get("PurchaseDate","")
+        if purchase_date.strip():
+            update_order(cursor, event["OrderID"],"PaymentDate", purchase_date)
         connection.commit()
         updated_rows = cursor.rowcount
         cursor.close()
         connection.close()
-    except:
+    except Exception as e:
+        print(e)
         return fail()
 
     return done({"AffectedRows":updated_rows})
 
-def update_order(cursor, orderID, shippingDate, purchaseDate):
-    cursor.execute("UPDATE public.\"Invoice\" SET \"ShippingDate\"= %s, \"purchaseDate\"= %s WHERE \"InvoiceID\"= %s ", (shippingDate, purchaseDate, orderID))
+def update_order(cursor, orderID, fieldName, fieldValue):
+    cursor.execute("UPDATE public.\"Invoice\" SET \"%s\" = %s WHERE \"InvoiceID\"= %s ", (AsIs(fieldName), fieldValue, orderID))
