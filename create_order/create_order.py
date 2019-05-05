6fe2cae68dbbde70539effb35c2e96bd56cc69b4
Healdr <Richard.Heald@airnz.co.nz>
2019-05-06 08:55:05 +1200

    Added method to generate invoice numbers

--- create_order/create_order.py
+++ create_order/create_order.py
@@ -2,7 +2,6 @@ import psycopg2
 import json
 import os
 from datetime import datetime
-from decimal import Decimal
 
 host  = os.environ['RDS_HOST']
 port = os.environ['PORT']
@@ -20,15 +19,6 @@ def done(response):
             'Access-Control-Allow-Origin': '*'
         }
     }
-def fail():
-    return {
-        'statusCode': '406',
-        'headers': {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Methods': '*',
-            'Access-Control-Allow-Origin': '*'
-        }
-    }
 
 def lambda_handler(event, context):
     connection = psycopg2.connect(user=username,
@@ -37,20 +27,19 @@ def lambda_handler(event, context):
                                   port=port,
                                   database=db_name)
     cursor = connection.cursor()
-    logtagInvoiceNumber = 'IN'+datetime.now().strftime("%d%m%y")
-    try:
-        invoice_id = create_invoice(cursor, int(event["customerID"]), logtagInvoiceNumber, event.get("purchaseOrderNumber",""), event.get("currency",""))
-        create_invoice_items(cursor, invoice_id, event["invoiceLines"])
-    except:
-        return fail()
+    count = generate_invoice_number(cursor)
+    logtagInvoiceNumber = 'IN'+datetime.now().strftime("%y%m%d")+'-'+str(count).zfill(2)
+    invoice_id = create_invoice(cursor, int(event["customerID"]), logtagInvoiceNumber)
+
+    create_invoice_items(cursor, invoice_id, event["invoiceLines"])
     connection.commit()
     cursor.close()
     connection.close()
     return done(json.dumps('{ \"InvoiceID\":\"'+str(invoice_id)+'\",  \"LogtagInvoiceNumber\":\"'+str(logtagInvoiceNumber)+'\"}'))
 
-def create_invoice(cursor, customerID, logtagInvoiceNumber, purchaseOrderNumber, currency):
+def create_invoice(cursor, customerID, logtagInvoiceNumber):
 
-    cursor.execute("INSERT into public.\"Invoice\" (\"CustomerID\", \"LogtagInvoiceNumber\", \"PurchaseOrderID\", \"Currency\")  VALUES ( %s, %s, %s, %s ) RETURNING \"InvoiceID\"", [customerID, logtagInvoiceNumber, purchaseOrderNumber, currency])
+    cursor.execute("INSERT into public.\"Invoice\" (\"CustomerID\", \"LogtagInvoiceNumber\")  VALUES ( %s, %s ) RETURNING \"InvoiceID\"", [customerID, logtagInvoiceNumber])
     row = cursor.fetchone()
     return row[0]
 
@@ -60,4 +49,11 @@ def create_invoice_items(cursor, invoiceID, invoiceLines):
         variationID = line["VariationID"]
         if(line["VariationID"] == "NULL"):
             variationID = "0"
-        cursor.execute("INSERT into public.\"InvoiceLine\" (\"InvoiceID\", \"Quantity\", \"ProductID\", \"Pricing\", \"VariationID\")  VALUES ( %s, %s, %s, %s, %s )", (invoiceID, int(line["Quantity"]), int(line["ProductID"]), Decimal(line["Price"]),int(variationID)))
+        cursor.execute("INSERT into public.\"InvoiceLine\" (\"InvoiceID\", \"Quantity\", \"ProductID\", \"Pricing\", \"VariationID\")  VALUES ( %s, %s, %s, %s, %s )", (invoiceID, int(line["Quantity"]), int(line["ProductID"]), int(line["Price"]),int(variationID)))
+
+def generate_invoice_number(cursor):
+    cursor.execute("SELECT COUNT(*) FROM public.\"Invoice\" WHERE \"CreatedDate\"> now() - interval '1 day' ")
+    row = cursor.fetchone()
+    if row is None:
+        return 0
+    return row[0];
