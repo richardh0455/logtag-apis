9807aba6662b73f24423a058e5028c6b03662ade
Healdr <Richard.Heald@airnz.co.nz>
2019-05-22 11:43:33 +1200

    Updating botocore naming

--- get_quicksight_url/get_quicksight_url.py
+++ get_quicksight_url/get_quicksight_url.py
@@ -1,4 +1,4 @@
-import botocore.session
+from botocore.session import Session
 import json
 import os
 
@@ -23,7 +23,7 @@ def fail():
     }
 
 def lambda_handler(event, context):
-    session = botocore.session.get_session()
+    session = Session()
     client = session.create_client("quicksight", region_name='ap-southeast-2')
     dashboard_url = client.get_dashboard_embed_url(AwsAccountId="276219036989", DashboardId="48187bc4-17ee-4a2c-b474-e69e184bd270", IdentityType="IAM", SessionLifetimeInMinutes=100, ResetDisabled=True, UndoRedoDisabled=True)
     result = '{'
