bcfc02901aa998e244a55ee4d870eca70eb3ef32
Healdr <Richard.Heald@airnz.co.nz>
2019-05-27 07:56:49 +1200

    Debugging

--- get_quicksight_embed_url/index.js
+++ get_quicksight_embed_url/index.js
@@ -50,7 +50,8 @@ const sendRes = (event, context, callback) => {
     AWS.config.update({
         region: process.env.AWS_REGION,
     });
-
+    var error = new Error(event);
+    callback(error);
     var accountId = context.invokedFunctionArn.match(/\d{3,}/)[0];
     var dashboardId = decodeURIComponent(event.dashboardId || event.queryStringParameters.dashboardId);
     var username = decodeURIComponent(event.username || event.queryStringParameters.username);
