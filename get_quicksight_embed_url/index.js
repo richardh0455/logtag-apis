374eb1a87cc305df83abf377714fb68aa124013f
Healdr <Richard.Heald@airnz.co.nz>
2019-05-30 10:10:26 +1200

    Refactoring Dashboard API, take 1

--- get_quicksight_embed_url/index.js
+++ get_quicksight_embed_url/index.js
@@ -6,7 +6,156 @@ exports.handler = function(event, context, callback) {
     return sendRes(event, context, callback);
 };
 
-const getDashboardURL = (accountId, dashboardId, callback) => {
+
+
+const sendRes = (event, context, callback) => {
+
+    AWS.config.update({
+        region: process.env.AWS_REGION,
+    });
+    var accountId = context.invokedFunctionArn.match(/\d{3,}/)[0];
+    var dashboardId = process.env.dashboardId ;
+    var username = event.username;
+    var sessionName = event.sessionName;
+    var idToken = event.Authorization;
+
+    if (!accountId) {
+        var error = new Error("accountId is unavailable");
+        callback(error);
+    }
+
+    if (!dashboardId) {
+        var error = new Error("dashboardId is unavailable");
+        callback(error);
+    }
+
+    if (!idToken) {
+        var error = new Error("idToken is unavailable");
+        callback(error);
+    }
+
+    if (!username) {
+        var error = new Error("username is unavailable");
+        callback(error);
+    }
+
+
+    var roleArn = 'arn:aws:iam::276219036989:role/Cognito_logtag_identity_poolAuth_Role'; // your cognito authenticated role arn here
+
+    var poolData = {
+        UserPoolId: 'ap-southeast-2_3e18SkGuR', // your user pool id here
+        ClientId: '5k4a2nd0euhv8bfaugepk8b4kf' // your app client id here
+    };
+    var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+
+    var userData = {
+        Username: username,
+        Pool: userPool
+    };
+
+    var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
+
+    var cognitoIdentity = new AWS.CognitoIdentity();
+    var stsClient = new AWS.STS();
+    var params = {
+        IdentityPoolId: 'ap-southeast-2:687f1ba1-1242-4f22-8adf-da49297c8005', // your identity pool id here
+        Logins: {
+            // your logins here
+            'cognito-idp.ap-southeast-2.amazonaws.com/ap-southeast-2_3e18SkGuR': idToken
+        }
+    };
+
+    cognitoIdentity.getId(params, function(err, data) {
+        authenticateAndGenerateURL(err, data, accountId, dashboardId, callback, roleArn, sessionName, idToken )
+    });
+}
+
+const authenticateAndGenerateURL = (err, data, accountId, dashboardId, callback, roleArn, sessionName, idToken) => {
+  if (err) console.log(err, err.stack);
+  else {
+      data.Logins = {
+          // your logins here
+          'cognito-idp.ap-southeast-2.amazonaws.com/ap-southeast-2_3e18SkGuR': idToken
+      };
+
+      cognitoIdentity.getOpenIdToken(data, function(err, openIdToken) {
+          getTokenAndGenerateURL(err, openIdToken, accountId, dashboardId, callback, roleArn, sessionName);
+      });
+  }
+
+}
+
+const getTokenAndGenerateURL = (err, openIdToken, accountId, dashboardId, callback, roleArn, sessionName) => {
+  if (err) {
+      console.log(err, err.stack);
+      callback(err);
+  } else {
+      let stsParams = {
+          RoleSessionName: sessionName,
+          WebIdentityToken: openIdToken.Token,
+          RoleArn: roleArn
+      }
+      stsClient.assumeRoleWithWebIdentity(stsParams, function(err, data) {
+          assumeRoleAndGenerateURL(err, data, accountId, dashboardId, callback, roleArn, sessionName);
+      });
+  }
+}
+
+const assumeRoleAndGenerateURL = (err, data, accountId, dashboardId, callback, roleArn, sessionName) => {
+  if (err) {
+      console.log(err, err.stack);
+      callback(err);
+  } else {
+      AWS.config.update({
+          region: 'us-east-1',
+          credentials: {
+              accessKeyId: data.Credentials.AccessKeyId,
+              secretAccessKey: data.Credentials.SecretAccessKey,
+              sessionToken: data.Credentials.SessionToken,
+              expiration: data.Credentials.Expiration
+          }
+      });
+
+      var registerUserParams = {
+          // required
+          AwsAccountId: accountId,
+          // can be passed in from api-gateway call
+          Email: 'xyz@xyz.com',
+          // can be passed in from api-gateway call
+          IdentityType: 'IAM',
+          // can be passed in from api-gateway call
+          Namespace: 'default',
+          // can be passed in from api-gateway call
+          UserRole: 'READER',
+          IamArn: roleArn,
+          SessionName: sessionName
+      };
+
+      var quicksight = new AWS.QuickSight();
+      quicksight.registerUser(registerUserParams, function(err, data) {
+          registerUserAndGenerateURL(err, data, accountId, dashboardId, callback)
+      });
+  }
+}
+
+const registerUserAndGenerateURL = (err, data, accountId, dashboardId, callback) => {
+  if (err) {
+      console.log(err, err.stack); // an error occurred
+      if (err.code && err.code === 'ResourceExistsException') {
+          generateURL(accountId, dashboardId, callback);
+      } else {
+          callback(err);
+      }
+  } else {
+      // successful response
+      setTimeout(function() {
+          generateURL(accountId, dashboardId, callback);
+      }, 2000);
+  }
+
+}
+
+const generateURL = (accountId, dashboardId, callback) => {
     AWS.config.update({
         region: process.env.AWS_REGION,
     });
@@ -43,145 +192,4 @@ const getDashboardURL = (accountId, dashboardId, callback) => {
         }
     });
 
-}
-
-const sendRes = (event, context, callback) => {
-
-    AWS.config.update({
-        region: process.env.AWS_REGION,
-    });
-    //let body = event.body;
-    var accountId = context.invokedFunctionArn.match(/\d{3,}/)[0];
-    var dashboardId = process.env.dashboardId ;
-    var username = event.username;
-    var sessionName = event.sessionName;
-    var idToken = event.Authorization;
-
-    if (!accountId) {
-        var error = new Error("accountId is unavailable");
-        callback(error);
-    }
-
-    if (!dashboardId) {
-        var error = new Error("dashboardId is unavailable");
-        callback(error);
-    }
-
-    if (!idToken) {
-        var error = new Error("idToken is unavailable");
-        callback(error);
-    }
-
-    if (!username) {
-        var error = new Error("username is unavailable");
-        callback(error);
-    }
-
-    /*if (!password) {
-        var error = new Error("password is unavailable");
-        callback(error);
-    }*/
-
-    var roleArn = 'arn:aws:iam::276219036989:role/Cognito_logtag_identity_poolAuth_Role'; // your cognito authenticated role arn here
-
-    /*var authenticationData = {
-        Username: username,
-        Password: password
-    };*/
-    //var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(authenticationData);
-
-    var poolData = {
-        UserPoolId: 'ap-southeast-2_3e18SkGuR', // your user pool id here
-        ClientId: '5k4a2nd0euhv8bfaugepk8b4kf' // your app client id here
-    };
-    var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
-
-    var userData = {
-        Username: username,
-        Pool: userPool
-    };
-
-    //var sessionName =  result.getIdToken().payload.sub;
-    var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
-
-    var cognitoIdentity = new AWS.CognitoIdentity();
-    var stsClient = new AWS.STS();
-    var params = {
-        IdentityPoolId: 'ap-southeast-2:687f1ba1-1242-4f22-8adf-da49297c8005', // your identity pool id here
-        Logins: {
-            // your logins here
-            'cognito-idp.ap-southeast-2.amazonaws.com/ap-southeast-2_3e18SkGuR': idToken
-        }
-    };
-
-    cognitoIdentity.getId(params, function(err, data) {
-        if (err) console.log(err, err.stack);
-        else {
-            data.Logins = {
-                // your logins here
-                'cognito-idp.ap-southeast-2.amazonaws.com/ap-southeast-2_3e18SkGuR': idToken
-            };
-
-            cognitoIdentity.getOpenIdToken(data, function(err, openIdToken) {
-                if (err) {
-                    console.log(err, err.stack);
-                    callback(err);
-                } else {
-                    let stsParams = {
-                        RoleSessionName: sessionName,
-                        WebIdentityToken: openIdToken.Token,
-                        RoleArn: roleArn
-                    }
-                    stsClient.assumeRoleWithWebIdentity(stsParams, function(err, data) {
-                        if (err) {
-                            console.log(err, err.stack);
-                            callback(err);
-                        } else {
-                            AWS.config.update({
-                                region: 'us-east-1',
-                                credentials: {
-                                    accessKeyId: data.Credentials.AccessKeyId,
-                                    secretAccessKey: data.Credentials.SecretAccessKey,
-                                    sessionToken: data.Credentials.SessionToken,
-                                    expiration: data.Credentials.Expiration
-                                }
-                            });
-
-                            var registerUserParams = {
-                                // required
-                                AwsAccountId: accountId,
-                                // can be passed in from api-gateway call
-                                Email: 'xyz@xyz.com',
-                                // can be passed in from api-gateway call
-                                IdentityType: 'IAM',
-                                // can be passed in from api-gateway call
-                                Namespace: 'default',
-                                // can be passed in from api-gateway call
-                                UserRole: 'READER',
-                                IamArn: roleArn,
-                                SessionName: sessionName
-                            };
-
-                            var quicksight = new AWS.QuickSight();
-                            quicksight.registerUser(registerUserParams, function(err, data) {
-                                if (err) {
-                                    console.log(err, err.stack); // an error occurred
-                                    if (err.code && err.code === 'ResourceExistsException') {
-                                        getDashboardURL(accountId, dashboardId, callback);
-                                    } else {
-                                        callback(err);
-                                    }
-                                } else {
-                                    // successful response
-                                    setTimeout(function() {
-                                        getDashboardURL(accountId, dashboardId, callback);
-                                    }, 2000);
-                                }
-                            });
-                        }
-                    });
-                }
-            });
-        }
-    });
 }
