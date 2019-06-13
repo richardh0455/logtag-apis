0a5bffb4c907d988feeee701bc9385a05948b7a3
Healdr <Richard.Heald@airnz.co.nz>
2019-06-13 12:46:47 +1200

    Removed billing address from create-customer

--- create_customer/create_customer.py
+++ create_customer/create_customer.py
@@ -26,13 +26,13 @@ def lambda_handler(event, context):
                                   port=port,
                                   database=db_name)
     cursor = connection.cursor()
-    customer_id = create_customer(cursor, event["Name"],event["EmailAddress"],event["BillingAddress"], event["Region"], event["PrimaryContact"] )
+    customer_id = create_customer(cursor, event["Name"],event["EmailAddress"], event["Region"], event["PrimaryContact"] )
     connection.commit()
     cursor.close()
     connection.close()
     return done(json.dumps('{ \"CustomerID\":\"'+str(customer_id)+'\"}'))
 
-def create_customer(cursor, name, email, billing_address, region, primaryContact):
-    cursor.execute("INSERT into public.\"Customer\" (\"Name\", \"Contact_Email\", \"Billing_Address\", \"Region\",\"Primary_Contact_Name\",\"Primary_Contact_Phone\",\"Primary_Contact_Fax\"  )  VALUES ( %s, %s, %s, %s, %s, %s, %s ) RETURNING \"CustomerID\"", (name, email, billing_address, region, primaryContact["Name"], primaryContact["Phone"], primaryContact["Fax"]))
+def create_customer(cursor, name, email, region, primaryContact):
+    cursor.execute("INSERT into public.\"Customer\" (\"Name\", \"Contact_Email\", \"Region\",\"Primary_Contact_Name\",\"Primary_Contact_Phone\",\"Primary_Contact_Fax\"  )  VALUES ( %s, %s, %s, %s, %s, %s ) RETURNING \"CustomerID\"", (name, email, region, primaryContact["Name"], primaryContact["Phone"], primaryContact["Fax"]))
     row = cursor.fetchone()
     return row[0]
