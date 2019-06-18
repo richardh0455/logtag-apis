50211e5feb88e17fa0707c258dd6cbc536d0103b
healdr <richardheald335@gmail.com>
2019-06-18 20:07:41 +1200

    Added VariationID to CustomerPriceList API

--- customer_price_list/customer_price_list.py
+++ customer_price_list/customer_price_list.py
@@ -38,9 +38,12 @@ def lambda_handler(event, context):
     try:
         cursor = connection.cursor()
         if event.get("Method","") =="GET":
-            value = get_price_lists(cursor, event["params"]["customer-id"], event["query"]["product-id"])
+            if event["query"].get("variation-id", "").strip() != "":
+                value = get_price_lists(cursor, event["params"]["customer-id"], event["query"]["product-id"], event["query"]["variation-id"])
+            else:
+                value = get_price_lists_without_variation(cursor, event["params"]["customer-id"], event["query"]["product-id"])
         if event.get("Method","")  =="POST":
-            value = create_price_item(cursor,  event["params"]["customer-id"], event["query"]["product-id"], event["body"]["Price"], event["body"]["Lower_Range"], event["body"]["Upper_Range"])
+            value = create_price_item(cursor,  event["params"]["customer-id"], event["query"]["product-id"], event["query"]["variation-id"], event["body"]["Price"], event["body"]["Lower_Range"], event["body"]["Upper_Range"])
         if event.get("Method","")  =="PUT":
             value = update_price_item(cursor, event["query"]["ID"], event["params"]["customer-id"], event["query"]["product-id"], event["body"]["Price"], event["body"]["Lower_Range"], event["body"]["Upper_Range"])
         if event.get("Method","")  =="DELETE":
@@ -55,8 +58,18 @@ def lambda_handler(event, context):
 
 
 
-def get_price_lists(cursor, customer_id, product_id):
-    cursor.execute("SELECT \"ID\", \"Price\", \"Lower_Range\", \"Upper_Range\"  FROM public.\"CustomerPriceList\" WHERE \"CustomerID\"= %s AND \"ProductID\" = %s", (customer_id, product_id,))
+def get_price_lists(cursor, customer_id, product_id, variation_id):
+    cursor.execute("SELECT \"ID\", \"Price\", \"Lower_Range\", \"Upper_Range\"  FROM public.\"CustomerPriceList\" WHERE \"CustomerID\"= %s AND \"ProductID\" = %s AND \"VariationID\" = %s", (customer_id, product_id, variation_id,))
+    list = []
+    for row in cursor.fetchall():
+        list.append(parse_price_item(row))
+    result = '['
+    result += ','.join(list)
+    result += ']'
+    return result
+
+def get_price_lists_without_variation(cursor, customer_id, product_id):
+    cursor.execute("SELECT \"ID\", \"Price\", \"Lower_Range\", \"Upper_Range\"  FROM public.\"CustomerPriceList\" WHERE \"CustomerID\"= %s AND \"ProductID\" = %s", (customer_id, product_id))
     list = []
     for row in cursor.fetchall():
         list.append(parse_price_item(row))
@@ -75,8 +88,8 @@ def parse_price_item(row):
     return result
 
 
-def create_price_item(cursor, customer_id, product_id, price, lower_range, upper_range):
-    cursor.execute("INSERT into public.\"CustomerPriceList\" (\"CustomerID\", \"ProductID\", \"Price\", \"Lower_Range\", \"Upper_Range\")  VALUES ( %s, %s, %s, %s, %s) RETURNING \"ID\"", (customer_id, product_id, price, lower_range, upper_range,))
+def create_price_item(cursor, customer_id, product_id, variation_id, price, lower_range, upper_range):
+    cursor.execute("INSERT into public.\"CustomerPriceList\" (\"CustomerID\", \"ProductID\", \"VariationID\", \"Price\", \"Lower_Range\", \"Upper_Range\")  VALUES ( %s, %s, %s, %s, %s, %s) RETURNING \"ID\"", (customer_id, product_id, variation_id, price, lower_range, upper_range,))
     row = cursor.fetchone()
     return {"ID":row[0]}
 
